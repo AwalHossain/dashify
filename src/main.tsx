e3702ea367e92008b8605ee72d9917776cb0f34a
Awalhossain <waqin21@gmail.com>
2025-05-17 12:41:04 +0600

    feat: Integrate React Query for data fetching and state management in the main application structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "flatpickr/dist/flatpickr.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -9,16 +10,29 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
+
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <AuthProvider>
-        <AlertProvider>
-          <AppWrapper>
-            <App />
-          </AppWrapper>
-        </AlertProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <AlertProvider>
+            <AppWrapper>
+              <App />
+            </AppWrapper>
+          </AlertProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 );
