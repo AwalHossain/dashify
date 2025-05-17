cf55d37f841b4a37535d4758dfa5de815c85ad7a
Awalhossain <waqin21@gmail.com>
2025-05-17 10:30:08 +0600

    refactor: Update Home component to use new Table component and remove commented-out code

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,6 +1,6 @@
 
 import PageMeta from "../../components/common/PageMeta";
-import BasicTables from "../Tables/BasicTables";
+import BasicTables from "../Tables/Table";
 
 export default function Home() {
   return (
@@ -10,29 +10,6 @@ export default function Home() {
         description="This is Ecommerce Dashboard page"
       />
       <BasicTables />
-      <div className="grid grid-cols-12 gap-4 md:gap-6">
-        {/* <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics />
-
-          <MonthlySalesChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <MonthlyTarget />
-        </div>
-
-        <div className="col-span-12">
-          <StatisticsChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <DemographicCard />
-        </div>
-
-        <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
-        </div> */}
-      </div>
     </>
   );
 }
