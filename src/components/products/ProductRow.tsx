f05a9b5ce96b0a6d0c24d2c4a1544a258661c702
Awalhossain <waqin21@gmail.com>
2025-05-17 09:34:38 +0600

    feat: Create ProductRow component for displaying product details with delete functionality

---
+++ src/components/products/ProductRow.tsx
@@ -0,0 +1,48 @@
+import { ColumnDefinition, Product } from "../../interface/product";
+import { TableCell, TableRow } from "../ui/table";
+
+interface ProductRowProps {
+    row: Product;
+    columns: ColumnDefinition[];
+    openDeleteModal: (slug: string, name: string) => void;
+    onViewDetails: (slug: string) => void;
+}
+
+export default function ProductRow({ row, columns, openDeleteModal, onViewDetails }: ProductRowProps) {
+    return (
+        <TableRow
+            key={row.id}
+            onClick={() => onViewDetails(row?.slug || '')}
+            className="cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800/50"
+        >
+            {columns.map(column => (
+                <TableCell
+                    key={`${row.id}-${column.key}`}
+                    className="px-5 py-4 text-gray-500 text-start text-theme-sm dark:text-gray-400 whitespace-nowrap"
+                >
+                    {column.render
+                        ? column.render(row[column.key as keyof Product], row)
+                        : String(row[column.key as keyof Product] || '')}
+                </TableCell>
+            ))}
+
+            <TableCell className="px-5 py-4 text-theme-sm whitespace-nowrap">
+                <button
+                    onClick={(e) => {
+                        e.stopPropagation(); // Prevent row click event
+                        const itemName = String(row[columns[0].key as keyof Product] || 'item');
+                        openDeleteModal(row?.slug || '', itemName);
+                    }}
+                    className="text-red-500 hover:text-red-700 focus:outline-none"
+                    title="Delete"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    </svg>
+                </button>
+            </TableCell>
+        </TableRow>
+    );
+}
